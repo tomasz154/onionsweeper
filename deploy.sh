52e5ce4f7fa62971b8725ad45d13413f9a99b3f5
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-07-15 12:14:19 +0200

    DEPLOY_USER

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-ssh $DEPLOY_HOST '(rm -Rf '$DEPLOY_DIRECTORY'/next || true) && mkdir '$DEPLOY_DIRECTORY'/next'
+ssh $DEPLOY_USER@$DEPLOY_HOST '(rm -Rf '$DEPLOY_DIRECTORY'/next || true) && mkdir '$DEPLOY_DIRECTORY'/next'
 
-scp -r build $DEPLOY_HOST:$DEPLOY_DIRECTORY/next/public
+scp -r build $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY/next/public
 
-ssh $DEPLOY_HOST '\
+ssh $DEPLOY_USER@$DEPLOY_HOST '\
     cd '$DEPLOY_DIRECTORY' && \
     rm -rf previous || true && \
     mv current previous || true && \
