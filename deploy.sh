b37047f7ecac918b5c744d2a7fb05b5c5b669d2d
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-07-15 12:25:57 +0200

    DEPLOY_HOST_FINGERPRINT

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo $DEPLOY_HOST_FINGERPRINT >> ~/.ssh/known_hosts
+
 ssh $DEPLOY_USER@$DEPLOY_HOST '(rm -Rf '$DEPLOY_DIRECTORY'/next || true) && mkdir '$DEPLOY_DIRECTORY'/next'
 
 scp -r build $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY/next/public
