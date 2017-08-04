7a50f7d0df027bdeed7c35f6f83a2579745dffaf
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-08-04 14:04:58 +0200

    trudniej

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ import {reset} from './actions';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-const mines = 10;
-const board = makeBoard(10, mines);
+const mines = 40;
+const board = makeBoard(16, mines);
 store.dispatch(reset(board, mines));
 
 ReactDOM.render(
