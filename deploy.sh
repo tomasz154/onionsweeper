bc19b034fe285e1f976ba8e90b120cad1ddad509
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-08-08 18:27:50 +0200

    sentry release hook

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,7 @@ ssh $DEPLOY_USER@$DEPLOY_HOST '\
     mv current previous || true && \
     mv next current
 '
+curl $SENTRY_RELEASE_HOOK_URL \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{"version": "'$VERSION'"}'
