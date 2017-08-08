b9609198023a42f21051ba2a2a5ed0661665e2f3
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-08-08 18:38:19 +0200

    sentry release hook

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,19 @@ ssh $DEPLOY_USER@$DEPLOY_HOST '\
     mv current previous || true && \
     mv next current
 '
-curl $SENTRY_RELEASE_HOOK_URL \
+curl https://sentry.io/api/0/organizations/$SENTRY_ORGANIZATION/releases/ \
   -X POST \
+  -H 'Authorization: Bearer '$SENTRY_KEY \
   -H 'Content-Type: application/json' \
-  -d '{"version": "'$VERSION'"}'
+  -d '{
+    "version": "'$VERSION'",
+    "projects": ["'$SENTRY_PROJECT'"]
+   }'
+
+curl https://sentry.io/api/0/organizations/$SENTRY_ORGANIZATION/releases/$VERSION/deploys/ \
+  -X POST \
+  -H 'Authorization: Bearer '$SENTRY_KEY \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "environment": "'$ENVIRONMENT'"
+   }'
