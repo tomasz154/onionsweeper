a54b52f9a591f9bf38446909d0d209826a288b26
Tomasz Tomkielski <tomasz@tomkielski.com>
2018-02-24 23:59:55 +0100

    report_deploy

--- report_deploy.sh
+++ report_deploy.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-ssh $DEPLOY_USER@$DEPLOY_HOST '(rm -Rf '$DEPLOY_DIRECTORY'/next || true) && mkdir '$DEPLOY_DIRECTORY'/next'
-
-scp -r build $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY/next/public
-
-ssh $DEPLOY_USER@$DEPLOY_HOST '\
-    cd '$DEPLOY_DIRECTORY' && \
-    rm -rf previous || true && \
-    mv current previous || true && \
-    mv next current
-'
 curl https://sentry.io/api/0/organizations/$SENTRY_ORGANIZATION/releases/ \
   -X POST \
   -H 'Authorization: Bearer '$SENTRY_KEY \
